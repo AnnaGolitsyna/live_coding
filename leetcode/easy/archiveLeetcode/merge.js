13f08b16902f37ee3cf0111d7981c7b80d05dad3
AnnaGolitsyna <110576gav@gmail.com>
2023-03-28 09:46:22 +0300

    cw2803

---
+++ leetcode/easy/archiveLeetcode/merge.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+
+const merge = function (nums1, m, nums2, n) {
+  nums2.forEach(element => {
+    nums1.unshift(element);
+  });
+  nums1.length = m + n;
+  nums1.sort((a, b) => a - b);
+};
+
+console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)); // [1,2,2,3,5,6]
+console.log(merge([1], 1, [], 0)); // [1]
+console.log(merge([0], 0, [1], 1)); // [1]
